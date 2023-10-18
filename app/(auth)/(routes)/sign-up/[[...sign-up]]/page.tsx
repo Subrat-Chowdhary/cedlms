d8811256e54b2f64d9d69101c3813e9734ceaae8
subrat-chowdhary <subratchowdhary@gmail.com>
2023-10-19 01:29:16 +0530

    Updated Logo and tweeked some other things

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,22 @@
 import { SignUp } from "@clerk/nextjs";
+import Image from "next/image";
+import loginSide from "@/public/VnilaLogo.svg"
  
 export default function Page() {
-  return <SignUp />;
+  return (
+    <>
+    <div className="flex">
+      <div className="flex flex-grow">
+        <Image 
+          src={loginSide}
+          alt="Logo"
+          className="w-full h-full"
+        />
+      </div>
+      <div className="flex">
+        <SignUp />
+      </div>
+    </div>
+  </>
+  );
 }
